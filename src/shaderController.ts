a1fa28048c6583cde7583de7aa6971b7a623a519
r21nomi <ryotakosu@gmail.com>
2018-06-11 23:07:36 +0900

    Remove window and show new one

--- src/shaderController.ts
+++ src/shaderController.ts
@@ -1,7 +1,15 @@
+import { ChildProcess } from "child_process";
+
 export class ShaderController {
+    private exec = require('child_process').exec;
+    private process: ChildProcess = null;
+
     public openShader(glslFileName: String) {
-        const exec = require('child_process').exec;
-        exec(`glslViewer ${glslFileName}`, (err, stdout, stderr) => {
+        if (this.process) {
+            // FIXME: An error will be thrown.
+            this.process.kill();
+        }
+        this.process = this.exec(`glslViewer ${glslFileName}`, (err, stdout, stderr) => {
             if (err) {
                 console.log(err);
             }
