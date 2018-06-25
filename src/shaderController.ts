6f1d58a9ea631ac2e29b0dd8505bcdab11bd734b
r21nomi <ryotakosu@gmail.com>
2018-06-26 01:47:56 +0900

    Fix bug an error happen after changing art

--- src/shaderController.ts
+++ src/shaderController.ts
@@ -1,19 +1,14 @@
-import { ChildProcess } from "child_process";
+import { ChildProcess } from 'child_process';
 
 export class ShaderController {
-    private exec = require('child_process').exec;
+
     private process: ChildProcess = null;
+    private childProcess = require('child_process');
 
     public openShader(glslFileName: String) {
         if (this.process) {
-            // FIXME: An error will be thrown.
             this.process.kill();
         }
-        this.process = this.exec(`glslViewer ${glslFileName}`, (err, stdout, stderr) => {
-            if (err) {
-                console.log(err);
-            }
-            console.log(stdout);
-        });
+        this.process = this.childProcess.spawn('glslViewer', [glslFileName]);
     }
 }
