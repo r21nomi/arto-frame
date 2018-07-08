810d81dbb946c374b46f85116a209f09fc2f8424
r21nomi <ryotakosu@gmail.com>
2018-07-09 00:49:39 +0900

    Remove firebase config

--- src/firebaseConfig.ts
+++
@@ -1,8 +0,0 @@
-export const firebaseConfig = {
-    apiKey: "AIzaSyBkjFZRQDTdRN2Q5R9we7Dd54ukXcrnOkQ",
-    authDomain: "arto-9b18c.firebaseapp.com",
-    databaseURL: "https://arto-9b18c.firebaseio.com",
-    projectId: "arto-9b18c",
-    storageBucket: "arto-9b18c.appspot.com",
-    messagingSenderId: "100783173461"
-};
